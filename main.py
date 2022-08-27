8ae258aa7625cf082f4beaffbe9861a102d1d4cc
brookehorizon <danieltashman17@gmail.com>
2022-08-27 21:23:41 +0300

    Updated

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import os, sys
+#! /bin/python3
+
+import sys
 from core.app import *
 from core.isconnected import IsConnected
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'app':
+
+    if sys.argv[1].lower() == 'app':
         App().run_app()
 
-
-    if sys.argv[1] == 'text':
+    elif sys.argv[1].lower() == 'text':
         if IsConnected(ip=sys.argv[2]).isconnected():
             print(f'{sys.argv[2]} is is connected !')
+
+    else:
+        print('./main.py app or ./main.py text 127.0.0.1')
+
+    
 
         
 
